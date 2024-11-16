eae3a9c3dce9ad05b2a96b365e4c641fc7c918b1
MellowKyler <dewerdk@gmail.com>
2024-11-16 09:28:51 -0600

    blargh

--- scratchpad.py
+++ scratchpad.py
@@ -1,8 +1,12 @@
 from bsky_utils import *
-from secret import login
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    did, nsid, rkey = decompose_uri(input("AT-URI: "))
-    service = get_service_endpoint(did)
-    session = get_session(login['uname'], login['pw'], service)
-    delete_record(session, service, nsid, rkey)
+    load_dotenv()
+    print(os.getenv("BSKY_UNAME"), os.getenv("BSKY_PW"))
+    print(resolve_handle(os.getenv("BSKY_UNAME")))
+    # did, nsid, rkey = decompose_uri(input("AT-URI: "))
+    # service = get_service_endpoint(did)
+    # session = get_session(os.getenv("BSKY_UNAME"), os.getenv("BSKY_PW"), service)
+    # delete_record(session, service, nsid, rkey)
